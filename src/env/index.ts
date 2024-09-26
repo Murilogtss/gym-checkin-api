3d8387a5250f5b5d462b9ba0c4be2539b056fb80
murilogtss <murilogtss123@gmail.com>
2024-09-26 00:14:02 -0300

    chore: add eslint configuration

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,7 +8,7 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
+if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
 
   throw new Error('Invalid environment variables')
