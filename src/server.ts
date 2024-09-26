7d5ecfaf2501e82a8cbfd046f5f0ef4122132ad4
murilogtss <murilogtss123@gmail.com>
2024-09-26 00:09:03 -0300

    chore: add environment configuration settings

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333
+  port: env.PORT
 }).then(() => {
-  console.log('🚀 HTTP Server running on http://localhost:3333')
+  console.log(`🚀 HTTP Server running on http://localhost:${env.PORT}`)
 })
