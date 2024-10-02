c2919c133cf795b356dcd10e9d6aa7d6675a6e12
murilogtss <murilogtss123@gmail.com>
2024-10-02 00:27:48 -0300

    test: validate successful user registration

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -5,6 +5,19 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
 describe('Register Use Case', () => {
+  it('should be able to register', async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'sHqFP@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password upon registration', async () => {
     const inMemoryUsersRepository = new InMemoryUsersRepository()
     const registerUseCase = new RegisterUseCase(inMemoryUsersRepository)
