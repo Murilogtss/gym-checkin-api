e0c531ae2b8aeb8e2731828d67a78e081bbf23c9
murilogtss <murilogtss123@gmail.com>
2024-10-04 00:43:41 -0300

    feat: add get user profile use case

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -11,6 +11,16 @@ export class PrismaUsersRepository implements UsersRepository {
     return user
   }
 
+  async findById(userId: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
--- src/repositories/users-repository.ts
+++ src/repositories/users-repository.ts
@@ -2,5 +2,6 @@ import type { Prisma, User } from '@prisma/client'
 
 export interface UsersRepository {
   findByEmail(email: string): Promise<User | null>
+  findById(userId: string): Promise<User | null>
   create(data: Prisma.UserCreateInput): Promise<User>
 }
