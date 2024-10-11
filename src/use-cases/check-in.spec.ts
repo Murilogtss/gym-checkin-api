cc8a5f4ff3e9afd76c169bb4bcc071aa6399d201
murilogtss <murilogtss123@gmail.com>
2024-10-11 00:09:37 -0300

    test: add checks for duplicate check-ins on the same day and allow check-ins on different days

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,7 +1,8 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-checkins-repository'
-import { beforeEach, it, describe, expect } from 'vitest'
+import { beforeEach, it, describe, expect, vi } from 'vitest'
 import { CheckInUseCase } from './check-in'
+import { afterEach } from 'node:test'
 
 let checkInsRepository: CheckInsRepository
 let sut: CheckInUseCase
@@ -10,12 +11,54 @@ describe('CheckIn Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to check in', async () => {
+    vi.setSystemTime(new Date(2024, 7, 10, 12, 0, 0))
+
     const { checkIn } = await sut.execute({
       userId: 'user-01',
       gymId: 'gym-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2024, 7, 10, 12, 0, 0))
+
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    await expect(
+      sut.execute({
+        userId: 'user-01',
+        gymId: 'gym-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2024, 7, 10, 12, 0, 0))
+
+    await sut.execute({
+      userId: 'user-01',
+      gymId: 'gym-01',
+    })
+
+    vi.setSystemTime(new Date(2024, 7, 11, 12, 0, 0))
+
+    const { checkIn } = await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
     })
 
     expect(checkIn.id).toEqual(expect.any(String))
